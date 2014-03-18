2eb2efd557a8663837509bcedad9d44706afb55f
Simon Nielsen <simon.c.nielsen@gmail.com>
2014-03-18 14:05:39 +0100

    Added libgdx project files
    Added the generated packages and classes from libgdx.

---
+++ MeteorDefense-desktop/src/com/esnefedroetem/meteordefense/Main.java
@@ -0,0 +1,16 @@
+package com.esnefedroetem.meteordefense;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "MeteorDefense";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MeteorDefense(), cfg);
+	}
+}
