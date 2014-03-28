4c046ba2ecfd5a6a2d9f3c9051f7867993ca6c5d
Andreas Pegelow <andpegelow@gmail.com>
2014-03-28 20:01:38 +0100

    Started implementing MeteorShower

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/MeteorShower.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/MeteorShower.java
@@ -1,5 +1,17 @@
 package com.esnefedroetem.meteordefense.model;
 
 public class MeteorShower {
+	public MeteorShower getInstance() {
+		return this;
+	}
+
+	public Meteor[] getNowFlyingMeteors() {
+
+		return null;
+	}
+
+	public void start() {
+
+	}
 
 }
