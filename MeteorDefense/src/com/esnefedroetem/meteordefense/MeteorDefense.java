981739d55bca5c273859ba9f5d43fb19d5485480
Jacob Gideflod <jacob@gideflod.se>
2014-03-28 21:03:53 +0100

    MeteoreDefense basic screen initialization

--- MeteorDefense/src/com/esnefedroetem/meteordefense/MeteorDefense.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/MeteorDefense.java
@@ -13,12 +13,38 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.esnefedroetem.meteordefense.screen.*;
 
 public class MeteorDefense extends Game implements PropertyChangeListener {
 	
-
+	private SplashScreen splashScreen;
+	private MainMenuScreen mainMenuScreen;
+	private ArmoryScreen armoryScreen;
+	private ArmoryDetailedScreen armoryDetaliedScreen;
+	//private GameScreen gameScreen;
+	private CarouselScreen carouselScreen;
+	
+	
 	@Override
 	public void create() {
+		init();
+		setScreen(splashScreen);
+	}
+	
+	/**
+	 * Initiate screens
+	 */
+	private void init(){
+		splashScreen = new SplashScreen();
+		splashScreen.addChangeListener(this);
+		mainMenuScreen = new MainMenuScreen();
+		mainMenuScreen.addChangeListener(this);
+		armoryScreen = new ArmoryScreen();
+		//armoryScreen.addChangeListener(this);
+		armoryDetaliedScreen = new ArmoryDetailedScreen();
+		//armoryDetaliedScreen.addChangeListener(this);
+		carouselScreen = new CarouselScreen();
+		carouselScreen.addChangeListener(this);
 		
 	}
 
