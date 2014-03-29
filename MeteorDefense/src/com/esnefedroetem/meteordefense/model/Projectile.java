c629741fed2a97d2f03a8f5cf2ed62cfbab49274
Simon Nielsen <simon.c.nielsen@gmail.com>
2014-03-29 18:52:43 +0100

    Implemented Projectile

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/Projectile.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/Projectile.java
@@ -1,5 +1,43 @@
 package com.esnefedroetem.meteordefense.model;
 
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
+
 public class Projectile {
 
+	private static final float SIZE = 2f;
+	private float speed = 50f; // Units per second
+	private Vector2 position;
+	private double angle;
+	private Circle bounds;
+	
+	public Projectile(double angle){
+		this.angle = angle;
+		bounds.x = position.x;
+		bounds.y = position.y;
+		bounds.radius = SIZE/2;
+	}
+	
+	public void move(float delta){
+		position.x = (float) (position.x + (speed * delta * Math.sin(angle)));
+		position.y = (float) (position.y + (speed * delta * Math.cos(angle)));
+		bounds.x = position.x;
+		bounds.y = position.y;
+	}
+	
+	public float getX(){
+		return position.x;
+	}
+	
+	public float getY(){
+		return position.y;
+	}
+	
+	public Circle getBounds(){
+		return bounds;
+	}
+	
+	public void setSpeed(float speed){
+		this.speed = speed;
+	}
 }
