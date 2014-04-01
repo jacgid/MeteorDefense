349570d01423f7a403972c4aa5345a0da27c43f6
emmalindholm <emmlindh@student.chalmers.se>
2014-04-01 17:43:13 +0200

    Collision between projectile and meteor does now work correctly

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/GameModel.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/GameModel.java
@@ -4,16 +4,15 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
-
-
 import com.badlogic.gdx.math.Rectangle;
 import com.esnefedroetem.meteordefense.Player;
 import com.esnefedroetem.meteordefense.util.Constants;
 
 /**
  * The GameModel handles all the gamelogic.
+ * 
  * @author Simon Nielsen
- *
+ * 
  */
 public class GameModel {
 
@@ -22,54 +21,62 @@ public class GameModel {
 	private MeteorShower meteorShower;
 	public static final float WIDTH = Constants.LOGIC_SCREEN_WIDTH;
 	public static final float HEIGHT = Constants.LOGIC_SCREEN_HEIGHT;
-	
+
 	private PropertyChangeSupport pcs;
-	
+
 	/**
 	 * Initializes the GameModel.
 	 */
-	public GameModel(Player player, City city){
+	public GameModel(Player player, City city) {
 		this.player = player;
 		this.meteorShower = city.getMeteorShower();
 		pcs = new PropertyChangeSupport(this);
 		meteorShower.start();
 	}
-	
+
 	/**
 	 * Updates the model
-	 * @param delta The time since this method was called last.
+	 * 
+	 * @param delta
+	 *            The time since this method was called last.
 	 */
-	public void update(float delta){
+	public void update(float delta) {
 		meteorShower.update(delta);
-		for(int i = 0; i < projectiles.size(); i++){
+		for (int i = 0; i < projectiles.size(); i++) {
 			projectiles.get(i).move(delta);
 		}
 		collisionControll();
 		removeProjectilesBeyondGameField();
 	}
-	
+
 	/**
 	 * Tells the player to shoot.
 	 */
-	public void shoot(float X, float Y){
+	public void shoot(float X, float Y) {
 		projectiles.add(player.shoot(X, Y));
 	}
-	
-	public void addChangeListener(PropertyChangeListener listener){
+
+	public void addChangeListener(PropertyChangeListener listener) {
 		pcs.addPropertyChangeListener(listener);
 	}
-	
+
 	public void collisionControll() {
-		for(Projectile projectile: projectiles) {
+		boolean collisionOccured = false;
+		for (Projectile projectile : projectiles) {
 			ArrayList<Meteor> meteors = meteorShower.getVisibleMeteors();
-			for (Meteor meteor: meteors) {
-			if (collisionOccurs(projectile, meteor)) {
-				handleCollision(projectile, meteor);
+			for (Meteor meteor : meteors) {
+				collisionOccured = collisionOccurs(projectile, meteor);
+				if (collisionOccured) {
+					handleCollision(projectile, meteor);
+					break;
+				}
+			}
+			if (collisionOccured) {
+				break;
 			}
 		}
-		}
 	}
-	
+
 	public void handleCollision(Projectile projectile, Meteor meteor) {
 		meteor.setLife(meteor.getLife() - projectile.getDamage());
 		if (meteor.getLife() <= 0) {
@@ -77,14 +84,15 @@ public class GameModel {
 		}
 		projectiles.remove(projectile);
 	}
+
 	public boolean collisionOccurs(Projectile projectile, Meteor meteor) {
 		return projectile.getBounds().overlaps(meteor.getBounds());
 	}
-	
+
 	public void removeProjectilesBeyondGameField() {
 		int length = projectiles.size();
-		
-		for(int i = 0; i < length; i++) {
+
+		for (int i = 0; i < length; i++) {
 			if (outOfBounds(projectiles.get(i))) {
 				projectiles.remove(projectiles.get(i));
 				length--;
@@ -92,20 +100,20 @@ public class GameModel {
 			}
 		}
 	}
-	
+
 	public boolean outOfBounds(MoveableGameObject object) {
 		float x = object.getX() + object.getBounds().radius;
 		float y = object.getY() + object.getBounds().radius;
-		
+
 		return (x < 0 || x > WIDTH || y > HEIGHT);
 	}
-	
-	public ArrayList<Projectile> getVisibleProjectiles(){
+
+	public ArrayList<Projectile> getVisibleProjectiles() {
 		return projectiles;
 	}
-	
-	public ArrayList<Meteor> getVisibleMeteors(){
+
+	public ArrayList<Meteor> getVisibleMeteors() {
 		return meteorShower.getVisibleMeteors();
 	}
-	
+
 }
