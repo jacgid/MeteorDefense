92ce4ecc3089898cb362ecc45565ab54476c918a
Jacob Gideflod <jacob@gideflod.se>
2014-04-03 15:33:50 +0200

    Created gameFactory and its methods

---
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/GameFactory.java
@@ -0,0 +1,38 @@
+package com.esnefedroetem.meteordefense;
+
+import com.esnefedroetem.meteordefense.screen.*;
+
+public class GameFactory {
+	
+	public static SplashScreen createSplashScreen(){
+		return new SplashScreen();
+	}
+	
+	public static MainMenuScreen createMainMenuScreen(){
+		return new MainMenuScreen();
+	}
+	
+	public static CarouselScreen createCarouselScreen(){
+		return new CarouselScreen();
+	}
+	
+	public static ArmoryScreen createArmoryScreen(){
+		return new ArmoryScreen();
+	}
+	
+	public static ArmoryDetailedScreen cretateArmoryDetailedScreen(){
+		return new ArmoryDetailedScreen();
+	}
+	
+	public static GameScreen createGameScreen(){
+		return new GameScreen();
+	}
+	
+	public static ScoreScreen createScoreScreen(){
+		return new ScoreScreen();
+	}
+	
+	
+	
+	
+}
