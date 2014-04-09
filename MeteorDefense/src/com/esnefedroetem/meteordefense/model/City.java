65aa3c963fe04e452939aa8d7b4827853a79c8f8
Simon Nielsen <simon.c.nielsen@gmail.com>
2014-04-09 14:56:11 +0200

    Fixed radioactive poison + new values of dmg,life

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/City.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/City.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.esnefedroetem.meteordefense.model.meteor.RadioactiveMeteor;
 import com.esnefedroetem.meteordefense.util.Constants;
 
 public class City {
@@ -17,6 +18,7 @@ public class City {
 	private Rectangle bounds = Constants.CITY_BOUNDS;
 	private MeteorShower meteorShower;
 	private List<Meteor> dashedMeteors = new ArrayList<Meteor>();
+	private float lastTime, lastDelta, totalTimePassed;
 	
 	public City(String name, int life, MeteorShower meteorShower, State state){
 		this.name = name;
@@ -67,5 +69,19 @@ public class City {
 	}
 	public void update(float delta) {
 		// TODO implement dashedMeteors continuously damaging city
+		totalTimePassed = totalTimePassed + delta;
+		if(totalTimePassed > 1){
+			int size = dashedMeteors.size();
+			for(int i = 0; i<size; i++){
+				RadioactiveMeteor radio = (RadioactiveMeteor)dashedMeteors.get(i);
+				if(radio.getDot()>=0){
+					life -= radio.getDot();
+				}else{
+					dashedMeteors.remove(i);
+					size = dashedMeteors.size();
+				}
+			}
+			totalTimePassed = 0;
+		}
 	}
 }
