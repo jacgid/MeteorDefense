243a162b5238789eb8220221bb087172452f3b5e
emmalindholm <emmlindh@student.chalmers.se>
2014-05-06 13:30:12 +0200

    Added implementation to initUpgrades() in EmptyItem to prevent nullpointer

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/armoryitem/EmptyItem.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/armoryitem/EmptyItem.java
@@ -1,5 +1,9 @@
 package com.esnefedroetem.meteordefense.model.armoryitem;
 
+import java.util.ArrayList;
+
+import com.esnefedroetem.meteordefense.model.Upgrade;
+
 public class EmptyItem extends AbstractArmoryItem {
 
 	
@@ -14,6 +18,11 @@ public class EmptyItem extends AbstractArmoryItem {
 
 	@Override
 	public void initUpgrades() {
+		ArrayList<Upgrade> upgrades = new ArrayList<Upgrade>();
+		for(int i = 0; i < 1; i++) {
+			upgrades.add(new Upgrade(0, 0, 0));
+		}
+		setUpgradeList(upgrades);
 		
 	}
 
