b11876619c82000ce3b31719a74c4129b279873d
emmalindholm <emmlindh@student.chalmers.se>
2014-05-07 13:32:40 +0200

    getNextUpgradeInfo() in AbstractArmoryItem now returns "No more upgrades available" when this occurs

--- MeteorDefense/src/com/esnefedroetem/meteordefense/model/armoryitem/AbstractArmoryItem.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/model/armoryitem/AbstractArmoryItem.java
@@ -8,10 +8,10 @@ import java.util.List;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.esnefedroetem.meteordefense.model.Upgrade;
 
-/** 
+/**
  * 
- *  @author Emma Lindholm
- *  
+ * @author Emma Lindholm
+ * 
  */
 
 public abstract class AbstractArmoryItem {
@@ -19,8 +19,9 @@ public abstract class AbstractArmoryItem {
 	public enum State {
 		LOCKED, UNLOCKED;
 	}
+
 	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-	
+
 	private State state;
 	private int power, sellValue, upgradeIndex;
 	private float cooldown;
@@ -28,17 +29,17 @@ public abstract class AbstractArmoryItem {
 	private List<Upgrade> upgrades;
 	protected String name, description;
 	public static final EmptyItem EMPTY_ITEM = new EmptyItem();
-	
+
 	public AbstractArmoryItem() {
 		init(State.LOCKED, 0);
 	}
-	
+
 	public void init(State state, int upgradeIndex) {
 		initUpgrades();
 		this.state = state;
 		setUpgradeIndex(upgradeIndex);
 	}
-	
+
 	public void upgrade() {
 		Upgrade upgrade = upgrades.get(upgradeIndex);
 		power = power + upgrade.getPowerIncrement();
@@ -46,11 +47,11 @@ public abstract class AbstractArmoryItem {
 		calculateSellValue(upgrade.getValue());
 		upgradeIndex++;
 	}
-	
+
 	public boolean hasUpgrade() {
 		return upgradeIndex < upgrades.size() - 1;
 	}
-	
+
 	private void reset() {
 		sellValue = 0;
 		upgradeIndex = 0;
@@ -58,10 +59,11 @@ public abstract class AbstractArmoryItem {
 		cooldown = 0;
 		upgrade();
 	}
+
 	public void addChangeListener(PropertyChangeListener pcl) {
 		pcs.addPropertyChangeListener(pcl);
 	}
-	
+
 	public void removeChangeListener(PropertyChangeListener pcl) {
 		pcs.removePropertyChangeListener(pcl);
 	}
@@ -69,99 +71,105 @@ public abstract class AbstractArmoryItem {
 	public PropertyChangeSupport getPropertyChangeSupport() {
 		return pcs;
 	}
-	
+
 	public State getState() {
 		return state;
 	}
-	
+
 	public void setState(State state) {
 		this.state = state;
 		if (state == State.LOCKED) {
 			reset();
 		}
 	}
-	
+
 	public int getPower() {
 		return power;
 	}
-		
+
 	public float getCooldown() {
 		return cooldown;
 	}
-	
+
 	public int getUpgradeIndex() {
 		return upgradeIndex;
 	}
-	
+
 	public void setUpgradeIndex(int upgradeIndex) {
 		this.upgradeIndex = 0;
-		for(int i = 0; i < upgradeIndex; i++) {
+		for (int i = 0; i < upgradeIndex; i++) {
 			upgrade();
 		}
 	}
-	
+
 	public void setUpgradeList(ArrayList<Upgrade> upgrades) {
 		this.upgrades = upgrades;
 	}
-	
+
 	private void calculateSellValue(int value) {
-		sellValue += value/2;
+		sellValue += value / 2;
 	}
-	
+
 	public int getPurchaseValue() {
 		return upgrades.get(0).getValue();
 	}
-	
+
 	public int getSellValue() {
-		return sellValue;		
+		return sellValue;
 	}
-	
+
 	public int getNextUpgradeValue() {
 		return upgrades.get(upgradeIndex).getValue();
 	}
-	
+
 	public void act() {
-		if(TimeUtils.timeSinceMillis(lastUsed) > cooldown * 1000) {
+		if (TimeUtils.timeSinceMillis(lastUsed) > cooldown * 1000) {
 			lastUsed = TimeUtils.millis();
 			performAct();
 		}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	@Override
-	public boolean equals(Object o){
-		if(o instanceof AbstractArmoryItem){
+	public boolean equals(Object o) {
+		if (o instanceof AbstractArmoryItem) {
 			return name.equals(((AbstractArmoryItem) o).getName());
 		}
 		return false;
 	}
-	
+
 	public abstract void performAct();
-	
+
 	public abstract void initUpgrades();
 
 	public abstract void update(float delta);
 
 	public String getNextUpgradeInfo() {
-		if(upgrades.get(upgradeIndex).getPowerIncrement() == 0) {
-			return "Cooldown: " + cooldown + upgrades.get(upgradeIndex).getCooldownDecrement() + " sec";
+		if (hasUpgrade()) {
+			if (upgrades.get(upgradeIndex).getPowerIncrement() == 0) {
+				return "Cooldown: " + cooldown
+						+ upgrades.get(upgradeIndex).getCooldownDecrement()
+						+ " sec";
+			}
+			return "Power: " + power
+					+ upgrades.get(upgradeIndex).getPowerIncrement();
 		}
-		return "Power: " + power + upgrades.get(upgradeIndex).getPowerIncrement();
+		return "No more upgrades available";
 	}
-		
+
 }
