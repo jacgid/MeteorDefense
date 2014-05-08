f62a7d001ed2190cd1049bcc31ffdda3973fc013
Andreas Pegelow <andpegelow@gmail.com>
2014-05-08 15:20:18 +0200

    New BG on gamescreen

--- MeteorDefense/src/com/esnefedroetem/meteordefense/renderer/GameRenderer.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/renderer/GameRenderer.java
@@ -62,7 +62,7 @@ public class GameRenderer {
 	private ShapeRenderer debugRenderer = new ShapeRenderer();
 	private boolean spritesLoaded = false;
 	
-	private String[] textures = {"meteor1.png", "projectile1.png", "cannonbarrel.png", "city1.png", "toolbar1.png", "MDBG.png"};
+	private String[] textures = {"meteor1.png", "projectile1.png", "cannonbarrel.png", "city1.png", "toolbar1.png", "MDBG1.png"};
 	private Texture meteorTexture, gunbarrel, projectileTexture, cannonTexture, cityTexture, toolbarTexture, bgTexture;
 	private Sprite projectileSprite, meteorSprite, cannonSprite, citySprite, toolbarSprite, bgSprite;
 	private Rectangle viewport;
