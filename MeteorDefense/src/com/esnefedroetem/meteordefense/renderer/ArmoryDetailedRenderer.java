f79ac125231e37e46388cb07a2ddc1b0ef67da00
emmalindholm <emmlindh@student.chalmers.se>
2014-05-20 11:51:39 +0200

    Added the coin texture to ArmoryDetailedRenderer

--- MeteorDefense/src/com/esnefedroetem/meteordefense/renderer/ArmoryDetailedRenderer.java
+++ MeteorDefense/src/com/esnefedroetem/meteordefense/renderer/ArmoryDetailedRenderer.java
@@ -37,7 +37,7 @@ public class ArmoryDetailedRenderer {
 	private Image itemImage, upgradeImage;
 	private AssetsLoader assetsLoader = AssetsLoader.getInstance();
 	private Table descriptionTable, upgradeTable;
-	private final float STANDARD_PADDING = Gdx.graphics.getWidth()/50f;
+	private final float STANDARD_PADDING = Gdx.graphics.getWidth()/25f;
 
 	public enum ArmoryDetaliedEvent {
 		ARMORY_DETAILED_BACK_PRESSED, ARMORY_DETAILED_ITEM_UPGRADED, ARMORY_DETAILED_TRADE_BUTTON_PRESSED
@@ -69,15 +69,15 @@ public class ArmoryDetailedRenderer {
 		textLabelStyle.font = assetsLoader.getExtraSmallFont();
 		assetsLabel = new Label("", textLabelStyle);
 		
-		assetsTable.add(assetsLabel).padRight(STANDARD_PADDING).padLeft(STANDARD_PADDING).right().expandX();
+		assetsTable.add(assetsLabel).padRight(STANDARD_PADDING/2).padLeft(STANDARD_PADDING/2).right().expandX();
 		
 		// coinImage
-		Image coinImage = new Image(assetsLoader.getTexture("PlayButton.png"));
+		Image coinImage = new Image(assetsLoader.getTexture("coin.png"));
 		
 		assetsTable.add(coinImage)
 		.width(Gdx.graphics.getWidth() * 0.05F)
 		.height(Gdx.graphics.getWidth() * 0.05F
-				* coinImage.getHeight() / coinImage.getWidth()).right();
+				* coinImage.getHeight() / coinImage.getWidth()).right().padRight(STANDARD_PADDING/2);
 
 		table.add(assetsTable).width(Gdx.graphics.getWidth());
 
@@ -89,13 +89,13 @@ public class ArmoryDetailedRenderer {
 		nameLabelStyle.font = assetsLoader.getSmallFont();
 		nameLabel = new Label("", nameLabelStyle);
 
-		table.add(nameLabel).left().pad(STANDARD_PADDING);
+		table.add(nameLabel).left().padLeft(STANDARD_PADDING).padTop(STANDARD_PADDING/2);
 		table.row();
 		
 		Image divider1 = new Image(assetsLoader.getTexture("divider.png"));
 		table.add(divider1).width(Gdx.graphics.getWidth())
 				.height(Gdx.graphics.getWidth() * 0.9F
-						* divider1.getHeight() / divider1.getWidth());
+						* divider1.getHeight() / divider1.getWidth()).padBottom(STANDARD_PADDING);
 
 		table.row();
 
@@ -105,15 +105,15 @@ public class ArmoryDetailedRenderer {
 		// itemImage
 		itemImage = new Image(assetsLoader.getTexture("PlayButton.png"));
 		descriptionTable.add(itemImage)
-				.width(Gdx.graphics.getWidth() * 0.25F)
-				.height(Gdx.graphics.getWidth() * 0.25F
-						* itemImage.getHeight() / itemImage.getWidth()).top().left().pad(STANDARD_PADDING);
+		.width(Gdx.graphics.getWidth() * 0.25F)
+		.height(Gdx.graphics.getWidth() * 0.25F
+				* itemImage.getHeight() / itemImage.getWidth()).top().left().padLeft(STANDARD_PADDING).padRight(STANDARD_PADDING);
 
 		// descriptionLabel
 		descriptionLabel = new Label("", textLabelStyle);
 		descriptionLabel.setWrap(true);
 
-		descriptionTable.add(descriptionLabel).width(Gdx.graphics.getWidth()/1.7f).right();
+		descriptionTable.add(descriptionLabel).width(Gdx.graphics.getWidth()/1.6f).top().left();
 
 		table.add(descriptionTable).left().top().expand();
 		table.row();
@@ -133,17 +133,16 @@ public class ArmoryDetailedRenderer {
 		Image divider2 = new Image(assetsLoader.getTexture("divider.png"));
 		table.add(divider2).width(Gdx.graphics.getWidth())
 				.height(Gdx.graphics.getWidth() * 0.9F
-						* divider2.getHeight() / divider2.getWidth()).row();
+						* divider2.getHeight() / divider2.getWidth()).padTop(STANDARD_PADDING).padBottom(STANDARD_PADDING).row();
 		// upgradeTable
 		upgradeTable = new Table();
-		//upgradeTable.setBackground(new TextureRegionDrawable(new TextureRegion(assetsLoader.getTexture("bluetransparentpixel.png"))));
 		
 		// upgradeImage
 		upgradeImage = new Image(assetsLoader.getTexture("PlayButton.png"));
 		upgradeTable.add(upgradeImage)
 				.width(Gdx.graphics.getWidth() * 0.15F)
 				.height(Gdx.graphics.getWidth() * 0.15F
-						* upgradeImage.getHeight() / upgradeImage.getWidth()).top().left().pad(STANDARD_PADDING);
+						* upgradeImage.getHeight() / upgradeImage.getWidth()).top().left().padLeft(STANDARD_PADDING);
 
 		
 		// upgradeLabel
@@ -151,7 +150,7 @@ public class ArmoryDetailedRenderer {
 
 		upgradeTable.add(upgradeLabel).right();
 		
-		table.add(upgradeTable).padLeft(STANDARD_PADDING).left().top().expandY();
+		table.add(upgradeTable).left().top().expandY();
 		table.row();
 
 		// buttonTable
@@ -181,7 +180,7 @@ public class ArmoryDetailedRenderer {
 
 		float aspect = upgradeButton.getHeight() / upgradeButton.getWidth();
 		buttonTable.add(upgradeButton).left().width(Gdx.graphics.getWidth() * 0.5F)
-				.height(Gdx.graphics.getWidth() * 0.5F * aspect).padTop(STANDARD_PADDING * 3);
+				.height(Gdx.graphics.getWidth() * 0.5F * aspect).padTop(STANDARD_PADDING * 1.5f);
 
 		// tradeButton
 		TextButtonStyle tradeButtonStyle = new TextButtonStyle(textButtonStyle);
@@ -199,7 +198,7 @@ public class ArmoryDetailedRenderer {
 		});
 
 		buttonTable.add(tradeButton).right().width(Gdx.graphics.getWidth() * 0.5F)
-				.height(Gdx.graphics.getWidth() * 0.5F * aspect).padTop(STANDARD_PADDING * 3);
+				.height(Gdx.graphics.getWidth() * 0.5F * aspect).padTop(STANDARD_PADDING * 1.5f);
 
 		table.add(buttonTable).bottom().height(Gdx.graphics.getHeight() * 0.14f);
 		
@@ -296,7 +295,7 @@ public class ArmoryDetailedRenderer {
 				.height(Gdx.graphics.getWidth() * 0.25F
 						* itemImage.getHeight() / itemImage.getWidth()).top().left().padLeft(STANDARD_PADDING).padRight(STANDARD_PADDING);
 
-		descriptionTable.add(descriptionLabel).width(Gdx.graphics.getWidth()/1.5f).right().top();
+		descriptionTable.add(descriptionLabel).width(Gdx.graphics.getWidth()/1.6f).top().left();
 	}
 	
 	public void setUpgradeImage(String textureName) {
@@ -307,8 +306,8 @@ public class ArmoryDetailedRenderer {
 		upgradeTable.add(upgradeImage)
 		.width(Gdx.graphics.getWidth() * 0.15F)
 		.height(Gdx.graphics.getWidth() * 0.15F
-				* upgradeImage.getHeight() / upgradeImage.getWidth()).top().left().pad(STANDARD_PADDING);
+				* upgradeImage.getHeight() / upgradeImage.getWidth()).top().left().padLeft(STANDARD_PADDING);
 
-		upgradeTable.add(upgradeLabel).right();
+		upgradeTable.add(upgradeLabel).padLeft(STANDARD_PADDING).right();
 	}
 }
